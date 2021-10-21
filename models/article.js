ab000c305757621e2a2521f7426177d08ff6cf54
McCambley <jakemccamb16@gmail.com>
2021-10-21 17:47:10 -0400

    Dev: create models

---
+++ models/article.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const articleSchema = new Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('article', articleSchema);
--- models/user.js
+++ models/user.js
@@ -1,1 +1,23 @@
 import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
