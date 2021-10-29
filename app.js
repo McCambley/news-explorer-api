33f60423387ac93375299b7297e9726ca3144cab
McCambley <jakemccamb16@gmail.com>
2021-10-29 00:12:59 -0400

    Fix: remove development helpers

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ const error = require('./middleware/error');
 // const auth = require('./middleware/auth');
 
 // // routes
-// const users = require('./routes/users');
-// const articles = require('./routes/articles');
-// const signin = require('./routes/signin');
-// const signup = require('./routes/signup');
 const mainRoute = require('./routes/index');
 
 // helpers
@@ -24,7 +20,6 @@ const { PORT = 3000, MONGO_URL, NODE_ENV } = process.env;
 const app = express();
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/newsexplorer');
-console.log(NODE_ENV);
 
 app.use(cors());
 app.options('*', cors());
@@ -37,14 +32,7 @@ app.use(requestLogger);
 // limit requests
 app.use(limiter);
 
-// app.use('/signup', signup);
-// app.use('/signin', signin);
-
-// // protect routes
-// app.use(auth);
-
-// app.use('/users', users);
-// app.use('/articles', articles);
+// routing
 app.use('/', mainRoute);
 
 // log errors
