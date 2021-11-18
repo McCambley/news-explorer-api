77689bfef81de3a3b078f4c228eb5128bbcc9dae
McCambley <jakemccamb16@gmail.com>
2021-11-18 11:47:08 -0500

    Fix: actually sort by date

--- controllers/articles.js
+++ controllers/articles.js
@@ -3,7 +3,7 @@ const ErrorHandler = require('../helpers/error');
 
 const getArticles = (req, res, next) => {
   Article.find({ owner: req.user._id })
-    .sort('date')
+    .sort('-date')
     .then((articles) => {
       res.send({ data: articles });
     })
